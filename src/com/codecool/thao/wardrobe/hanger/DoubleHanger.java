63fa4ec7e8bd073192544cea4c39b57b6826cb55
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-02-25 22:34:18 +0100

    added documentation

--- src/com/codecool/thao/wardrobe/hanger/DoubleHanger.java
+++ src/com/codecool/thao/wardrobe/hanger/DoubleHanger.java
@@ -1,5 +1,9 @@
 package com.codecool.thao.wardrobe.hanger;
 
+/**
+ * one that can accommodate a shirt or a blouse and
+ * at the same time trousers or a skirt too.
+ */
 public class DoubleHanger extends Hanger {
     public DoubleHanger() {
         this.limit = 2;
--- src/com/codecool/thao/wardrobe/hanger/SingleHanger.java
+++ src/com/codecool/thao/wardrobe/hanger/SingleHanger.java
@@ -1,5 +1,8 @@
 package com.codecool.thao.wardrobe.hanger;
 
+/**
+ * one that can accommodate a single shirt or blouse only
+ */
 public class SingleHanger extends Hanger {
     public SingleHanger() {
         this.limit = 1;
