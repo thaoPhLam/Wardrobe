a4bc98cd15ba1de86d7f9c5afbe76bc8a2058175
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-02-27 16:58:19 +0100

    uncommented

--- src/com/codecool/thao/wardrobe/app/Main.java
+++ src/com/codecool/thao/wardrobe/app/Main.java
@@ -2,7 +2,6 @@ package com.codecool.thao.wardrobe.app;
 
 import com.codecool.thao.wardrobe.Wardrobe;
 import com.codecool.thao.wardrobe.cloth.Cloth;
-import com.codecool.thao.wardrobe.cloth.ClothPart;
 import com.codecool.thao.wardrobe.cloth.ClothType;
 import com.codecool.thao.wardrobe.hanger.DoubleHanger;
 import com.codecool.thao.wardrobe.hanger.Hanger;
@@ -11,15 +10,16 @@ import com.codecool.thao.wardrobe.hanger.SingleHanger;
 public class Main {
     public static void main(String[] args) {
         Wardrobe wardrobe = new Wardrobe();
-        Cloth blouse = new Cloth(ClothType.BLOUSE, ClothPart.UPPER);
-        Cloth jeans = new Cloth(ClothType.JEANS, ClothPart.BOTTOM);
-        Cloth shirt = new Cloth(ClothType.SHIRT, ClothPart.UPPER);
-        Cloth skirt = new Cloth(ClothType.SKIRT, ClothPart.BOTTOM);
+
+        Cloth blouse = new Cloth(ClothType.BLOUSE);
+        Cloth jeans = new Cloth(ClothType.JEANS);
+        Cloth shirt = new Cloth(ClothType.SHIRT);
+        Cloth skirt = new Cloth(ClothType.SKIRT);
 
         Hanger singleHanger = new SingleHanger();
         singleHanger.putOntoCloth(blouse);
         singleHanger.putOntoCloth(shirt);
-        singleHanger.putOntoCloth(skirt);  //TODO: print out: it's not a TOP
+        singleHanger.putOntoCloth(skirt);
 
         Hanger doubleHanger = new DoubleHanger();
         doubleHanger.putOntoCloth(jeans);
@@ -27,17 +27,16 @@ public class Main {
 
         doubleHanger.putOntoCloth(shirt);
         doubleHanger.putOntoCloth(blouse);
-        doubleHanger.putOntoCloth(skirt);  //TODO: reordering...
+        doubleHanger.putOntoCloth(skirt);
 
         wardrobe.hangHanger(singleHanger);
         wardrobe.hangHanger(doubleHanger);
 
         wardrobe.isTherePlaceFor();
-        //wardrobe.printOutHangers();
 
-        System.out.println("I'm taking this out: " + wardrobe.takeOutCloth(3));
-        //System.out.println("I'm taking ALL out: " + doubleHanger.takeAllClothingOff());
-        //System.out.println("I'm taking ALL out: " + singleHanger.takeAllClothingOff());
+        System.out.println("I'm taking this out: " + wardrobe.takeOutCloth(2));
+        System.out.println("I'm taking ALL out: " + doubleHanger.takeAllClothingOff());
+        System.out.println("I'm taking ALL out: " + singleHanger.takeAllClothingOff());
 
     }
 }
